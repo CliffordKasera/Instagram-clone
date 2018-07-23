b3049c92340007edeb6dd0ced09817a0ca2bd195
Blankphrase <ckasera6486@gmail.com>
2018-07-23 10:19:41 +0300

    registering models with the admin

--- instagramClone/admin.py
+++ instagramClone/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile,Image,Comments
+
+
+# Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comments)
