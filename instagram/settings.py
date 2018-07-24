8e59f88e63f08e5e172f3cec7f79a224d63a3b3b
Blankphrase <ckasera6486@gmail.com>
2018-07-24 07:20:06 +0300

    define static path

--- instagram/settings.py
+++ instagram/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+
+
 TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
@@ -122,3 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
