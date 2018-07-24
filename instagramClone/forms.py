19407e22d2f3a04fc94b49004fa0e21a0c1c0934
Blankphrase <ckasera6486@gmail.com>
2018-07-24 15:24:42 +0300

    creation of forms for the models

--- instagramClone/forms.py
+++ instagramClone/forms.py
@@ -1,14 +1,8 @@
 from django import forms
+from pyuploadcare.dj.forms import FileWidget
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Image, Profile, Comments
-
-class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text = 'Required')
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'confirmpassword')
 
 class ImageForm(forms.ModelForm):
 
