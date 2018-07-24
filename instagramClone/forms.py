14985f9b26cfc06622c15a531f537351b570081e
Blankphrase <ckasera6486@gmail.com>
2018-07-24 19:12:35 +0300

    removing signup form

--- instagramClone/forms.py
+++ instagramClone/forms.py
@@ -8,7 +8,7 @@ class ImageForm(forms.ModelForm):
 
     class Meta:
         model = Image
-        exclude = ['likes', 'upload_date', 'user']
+        exclude = ['likes', 'upload_date']
     
 class ProfileForm(forms.ModelForm):
     class Meta:
