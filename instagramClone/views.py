7988f6ef40c80489a1f9365d47967e5652b020d9
Blankphrase <ckasera6486@gmail.com>
2018-07-28 17:08:29 +0300

    creation of like, follow and comment view functions

--- instagramClone/views.py
+++ instagramClone/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
-from .models import Image, Profile, Comments
+from .models import Image, Profile, Comments, Likes
 from .forms import ImageForm, ProfileForm, CommentForm
 from django.contrib.auth.models import User
+from friendship.exceptions import AlreadyExistsError
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 
@@ -13,24 +14,29 @@ from django.template.loader import render_to_string
 
 def home(request):
     title = 'Instagram'
+    current_user = request.user
     images = Image.get_all_images()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all()
+    profile = Profile.objects.all()
     
-    return render(request, 'instagram/index.html', {'images':images,'title':title})
+    return render(request, 'instagram/index.html', locals())
 
 
 @login_required(login_url='/accounts/login')
 def upload_image(request):
+    current_user = request.user
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             upload = form.save(commit=False)
-            upload.profile = request.user
+            upload.profile = current_user
             upload.save()
             return redirect('edit_profile', username=request.user)
     else:
         form = ImageForm()
     
-    return render(request, 'registration/upload_image.html', {'form':form})
+    return render(request, 'registration/upload_image.html', locals())
 
 @login_required(login_url='/accounts/login')
 def edit_profile(request, username):
@@ -40,9 +46,13 @@ def edit_profile(request, username):
     except:
         profile_details = Profile.filter_by_id(user.id)
     images = Image.get_profile_images(user.id)
-    title = f'@{user.username} Instagram photos and videos'
+    follower = len(Follow.objects.followers(user))
+    following = len(Follow.objects.following(user))
+    users=User.objects.all()
+    users_following=Follow.objects.following(request.user)
+    title = f'@{user.username} Instagram photos'
 
-    return render(request, 'registration/edit_profile.html', {'title':title, 'user':user, 'profile_details':profile_details, 'images':images})
+    return render(request, 'registration/edit_profile.html', locals())
 
 @login_required(login_url='/accounts/login')
 def editprofile(request):
@@ -56,7 +66,7 @@ def editprofile(request):
     else:
         form = ProfileForm()
 
-    return render(request, 'registration/profile.html', {'form':form})
+    return render(request, 'registration/profile.html', locals())
 
 @login_required(login_url='/accounts/login')
 def single_image(request, image_id):
@@ -82,7 +92,42 @@ def search(request):
         profiles = Profile.search_profile(search_term)
         message = f'{search_term}'
 
-        return render(request, 'search.html',{'message':message, 'profiles':profiles})
+        return render(request, 'instagram/search.html',{'message':message, 'profiles':profiles})
     else:
         message = 'Enter term to search'
-        return render(request, 'search.html', {'message':message})
+        return render(request, 'instagram/search.html', {'message':message})
+
+def comment(request,image_id):
+    current_user=request.user
+    profile = User.objects.get(username=current_user)
+    image = Image.objects.get(id=image_id)
+    comments = Comments.objects.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.user = current_user
+            comment.save()
+
+        return redirect('home')
+    else:
+        form = CommentForm()
+
+    return render(request, 'comment.html', locals())
+
+
+def follow(request,user_id):
+    users=User.objects.get(id=user_id)
+    follow = Follow.objects.add_follower(request.user, users)
+
+    return redirect('/edit_profile/', locals())
+
+
+def like(request, image_id):
+    current_user = request.user
+    image=Image.objects.get(id=image_id)
+    new_like,created= Likes.objects.get_or_create(liker=current_user, image=image)
+    new_like.save()
+
+    return redirect('home')
